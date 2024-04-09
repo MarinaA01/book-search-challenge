bdea8488695623169f7c37426ea4c1bc0db20202
Marina <marinajalonzo1@gmail.com>
2024-04-09 13:44:23 -0500

    Schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,28 @@
+const typeDefs = `
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  
+  type Query {
+    me: User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+  `
