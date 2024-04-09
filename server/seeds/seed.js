abe41f30b0adf140045591192db84d8fa63e2a22
Marina <marinajalonzo1@gmail.com>
2024-04-09 16:05:32 -0500

    Databases added.

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,4 +1,20 @@
 const db = require('../config/connection');
-const { Tech } = require('../models');
+const { User } = require('../models');
 const cleanDB = require('./cleanDB');
+
+const userData = require('./userData.json');
+
+db.once('open', async () => {
+    try {
+        await cleanDB('User', 'users');
+    
+        await User.create(userData);
+    
+        console.log('Users seeded successfully!');
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    });
+
 
