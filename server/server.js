4cc6d6e409a04529175cc9df025a88d1175853aa
Marina <marinajalonzo1@gmail.com>
2024-04-11 01:01:29 -0500

    Packages fixed.

--- server/server.js
+++ server/server.js
@@ -17,19 +17,20 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const startApolloServer = async () => {
-  await server.start();
-  // if we're in production, serve client/build as static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
+// if we're in production, serve client/dist as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.use(routes);
-
-  db.once('open', () => {
-    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
+} 
 
-}
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
+});
 
 startApolloServer();
