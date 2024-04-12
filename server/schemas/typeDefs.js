28073c9a555d7ab3336e9480955e34eaeb4ed024
Marina <marinajalonzo1@gmail.com>
2024-04-12 09:26:07 -0500

    Mutation added.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -21,8 +21,17 @@ const typeDefs = `
     me: User
   }
 
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: String!): User
+    removeBook(bookId: ID!): User
+  }
+
   type Auth {
     token: ID!
     user: User
   }
   `
+
+  module.exports = typeDefs;
