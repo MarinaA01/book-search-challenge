c78e0a283f4db94a8feb40d1529a0650d011d73b
Marina <marinajalonzo1@gmail.com>
2024-04-29 16:12:45 -0400

    Reverse changes.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://marinajalonzo1:<password>@cluster0.ovzyxdm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
